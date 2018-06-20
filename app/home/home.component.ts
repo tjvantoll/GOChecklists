1ddaa6278f10d920a1a2238f7e4035dcb28cb7c7
TJ VanToll <tj.vantoll@gmail.com>
2018-06-20 09:45:01 -0400

    Colors for the progressbar

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -19,6 +19,7 @@ export class HomeComponent implements OnInit {
   shinies: Shiny[];
   ownedCount;
   percentOwned;
+  progressbarColor;
 
   constructor(private shinyService: ShinyService) { }
 
@@ -52,6 +53,22 @@ export class HomeComponent implements OnInit {
 
     let percent = Math.round((owned / this.shinies.length) * 100);
     this.percentOwned = percent + "*," + (100 - percent) + "*";
+
+    this.progressbarColor = (percent <= 6) ? "#FB041E" :
+      (percent > 6 && percent <= 12) ? "#FD2222" :
+      (percent > 12 && percent <= 18) ? "#FC4926" :
+      (percent > 18 && percent <= 24) ? "#FC6628" :
+      (percent > 24 && percent <= 30) ? "#FE882A" :
+      (percent > 30 && percent <= 36) ? "#FFA52E" :
+      (percent > 36 && percent <= 42) ? "#FEC230" :
+      (percent > 42 && percent <= 48) ? "#FFDE34" :
+      (percent > 48 && percent <= 54) ? "#F4DE2B" :
+      (percent > 54 && percent <= 60) ? "#E7DD25" :
+      (percent > 60 && percent <= 66) ? "#DBDD1C" :
+      (percent > 66 && percent <= 72) ? "#CEDC18" :
+      (percent > 72 && percent <= 78) ? "#C3DC0E" :
+      (percent > 78 && percent <= 84) ? "#B6DC07" :
+      (percent > 84 && percent <= 90) ? "#A9DC03" : "#9ADA00";
   }
 
   toggleShinyOwned(args) {
