c0b4d778b4f97e8f554b163b6585eecbe7b8e241
TJ VanToll <tj.vantoll@gmail.com>
2018-12-17 17:22:46 -0500

    Removing unused import

--- src/app/shared/pokemon.service.ts
+++ src/app/shared/pokemon.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
 
 import { Pokemon } from "./pokemon.model";
 import { PokemonHelper } from "./pokemon-helper";
